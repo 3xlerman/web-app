e4921114d065bcf45930dab6333e8c40c3d72a0d
3xlerman <lermanworks@gmail.com>
2020-07-05 22:02:55 +0300

    feat: nginx server created

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	app := gin.Default()
+
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
