a97c6bdcf584bd2dac262f3ff7def840a2963d94
3xlerman <lermanworks@gmail.com>
2020-07-05 22:13:11 +0300

    feat: comments added

--- main.go
+++ main.go
@@ -8,14 +8,18 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
+
+	// Create nginx-server
 	app := gin.Default()
 
+	// Test-request
 	app.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
+	// Run application
 	err := app.Run()
 	if err != nil {
 		fmt.Println(err)
